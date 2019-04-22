19ad8d98eca4c542a416e3b7884c21f128a5fa40
Aisling Kathrine <Aisling@Aislings-MacBook-Air.local>
2019-04-22 12:25:25 +0200

    aisling færdig stevns yderligere viden

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//accordion
+// ACCORDION
 
 var acc = document.getElementsByClassName("accordion");
 var i;
@@ -15,7 +15,7 @@ for (i = 0; i < acc.length; i++) {
     });
 }
 
-//AKTUELT
+// AKTUELT
 
 let nyheder = [];
 document.addEventListener("DOMContentLoaded", start);
@@ -40,25 +40,6 @@ function start() {
             dest.appendChild(klon);
         })
 
-    }
-    getJson();
-}
-
-// OM OS
-let indhold;
-document.addEventListener("DOMContentLoaded", start);
-
-function start() {
-    async function getJson() {
-        let url = "http://lauravillumsen.com/kea/07-cms/kalklandet/wp//wp-json/wp/v2/pages/30"
-        let jsonData = await fetch(url);
-        indhold = await jsonData.json();
-        visIndhold();
-    }
-
-    function visIndhold() {
-        document.querySelector(".overskrift").textContent = indhold.title.rendered;
-        document.querySelector(".indhold").innerHTML = indhold.content.rendered;
     }
     getJson();
 }
