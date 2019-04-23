e49c6e9ea2841d79c43b69daad94b3c1a78b2e64
Irene Seedorff <iren3024@stud.kea.dk>
2019-04-23 11:56:28 +0200

    ændringer aktuelt

--- script.js
+++ script.js
@@ -15,34 +15,7 @@ for (i = 0; i < acc.length; i++) {
     });
 }
 
-//AKTUELT
 
-let nyheder = [];
-document.addEventListener("DOMContentLoaded", start);
-
-function start() {
-    async function getJson() {
-        let url = "http://lauravillumsen.com/kea/07-cms/kalklandet/wp//wp-json/wp/v2/nyhed"
-        let jsonData = await fetch(url);
-        nyheder = await jsonData.json();
-        visNyheder();
-    }
-
-    function visNyheder() {
-        let dest = document.querySelector(".nyheder");
-        let temp = document.querySelector("template");
-        nyheder.forEach(nyhed => {
-            let klon = temp.cloneNode(true).content;
-            klon.querySelector(".overskrift").innerHTML = nyhed.title.rendered;
-            klon.querySelector(".dato").innerHTML = nyhed.dato;
-            klon.querySelector(".tekst").innerHTML = nyhed.content.rendered;
-            klon.querySelector("img").src = nyhed.billede.guid;
-            dest.appendChild(klon);
-        })
-
-    }
-    getJson();
-}
 
 // OM OS
 let indhold;
